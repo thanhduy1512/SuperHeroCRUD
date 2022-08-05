4c7529812d1a3361d99b288d68b5e5e11022cc9c
thanhduy1512 <duongduyy1512@gmail.com>
2022-08-05 17:37:26 +0700

    initial

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHero2.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
+
